3e3e1c1b73a0078a893814baac8f9afd53bd562e
devsidak <er.sidak.singh@gmail.com>
2022-02-28 18:36:29 +0530

    Initial Commit

---
+++ components/Subheader.js
@@ -0,0 +1,11 @@
+
+function Subheader() {
+  return (
+    <section  className="flex w-full h-full image-cover bg-cover bg-center bg-no-repeat"
+    style={{backgroundImage:"url(/media/header1.jpg)"}}>
+    
+    </section>
+  )
+}
+
+export default Subheader;
